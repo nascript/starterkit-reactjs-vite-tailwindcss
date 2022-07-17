14899b877e51d5fccf62690988179e59cf6625fd
nascript <nascodefy@gmail.com>
2022-07-17 14:24:05 +0700

    starter project react with vite and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='App'>
+      <h1 className='bg-red-500'>NAS STARTER KIT REACT VITE TAILWINDCSS</h1>
+    </div>
+  )
+}
+
+export default App
